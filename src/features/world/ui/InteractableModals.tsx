52656c2e0cd8ef8fe7f14bb5659addb20d1ffee7
ispankzombiez <46771495+ispankzombiez@users.noreply.github.com>
2024-05-29 13:01:47 -0700

    poker modal work

--- src/features/world/ui/InteractableModals.tsx
+++ src/features/world/ui/InteractableModals.tsx
@@ -32,6 +32,7 @@ import { ChickenRescue } from "./portals/ChickenRescue";
 import { InlineDialogue } from "./TypingMessage";
 import { Label } from "components/ui/Label";
 import { FestivalOfColors } from "./portals/FestivalOfColors";
+import { PokerHouseModal } from "./PokerHouseModal"
 
 export type FanArtNPC = "fan_npc_1" | "fan_npc_2" | "fan_npc_3" | "fan_npc_4";
 
@@ -176,11 +177,7 @@ export const InteractableModals: React.FC<Props> = ({ id, scene }) => {
       </Modal>
 
       <Modal show={interactable === "poker_house_modal"} onHide={closeModal}>
-        <BoatModal
-          isOpen={interactable === "poker_house_modal"}
-          closeModal={closeModal}
-          id={id}
-        />
+        <PokerHouseModal closeModal = {closeModal}/>
       </Modal>
 
       <Modal show={interactable === "fat_chicken"} onHide={closeModal}>
